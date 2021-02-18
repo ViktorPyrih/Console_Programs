06a7a1a34c41de33f3157de6de09725227883e03
kallDun <igorxlonline24@gmail.com>
2021-02-18 11:01:58 +0200

    test 1 add

---
+++ Test 1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1
+{
+
+    struct Student : IComparable<Student>
+    {
+        public string name;
+        public string surName;
+        public DateTime birthday;
+        public double scholarship;
+        public Type type;
+
+        public Student(string name, string surName, DateTime birthday, double scholarship, Type type)
+        {
+            this.name = name;
+            this.surName = surName;
+            this.birthday = birthday;
+            this.scholarship = scholarship;
+            this.type = type;
+        }
+
+        public int CompareTo(Student other)
+        {
+            return birthday.CompareTo(other.birthday);
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surName + " " + type;
+        }
+    }
+
+    enum Type
+    {
+        Bachelor, Master
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student_1 = new Student("Ivan", "Ivanovich", new DateTime(2001, 03, 15), 1400, Type.Bachelor);
+            var student_2 = new Student("Ivan 2", "Ivanovich", new DateTime(2000, 04, 15), 1400, Type.Bachelor);
+            var student_3 = new Student("Ivan", "Ivanovich", new DateTime(1998, 03, 15), 1200, Type.Master);
+
+            Console.WriteLine(student_1.CompareTo(student_2));
+
+            var students = new Student[3] { student_1, student_2, student_3 };
+
+            Array.Sort(students);
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+        }
+    }
+}
