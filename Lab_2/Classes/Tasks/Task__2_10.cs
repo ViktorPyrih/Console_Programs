8e83f78406bd76534d2ef2d7ee16be679a1d4305
Denis Kozlovskyi <kozlovskyi.d@gmail.com>
2021-03-09 02:11:26 +0200

    Completed second task

---
+++ Lab_2/Classes/Tasks/Task__2_10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Lab_2.Classes.Tasks
+{
+    class Task__2_10 : BaseInit_TwoDim, IRunnable
+    {
+        public Task__2_10() :
+            base ("Додати по одному рядку після кожного парного рядка матриці"
+                    + "(тобто, кожного рядка, що у початковій"
+                    + "матриці мав парний номер)") { }
+
+        public void Run()
+        {
+            int originalSize = array.Length;
+
+            Array.Resize(ref array, array.Length + originalSize / 2);
+
+            InsertRows(array.Length - 1, originalSize - 1);
+
+            Console.WriteLine("New array:");
+            array.Print();
+        }
+
+        private void InsertRows(int newPosition, int oldPosition)
+        {
+            int[] rowToInsert = { 0, 0, 0 };
+
+            while (oldPosition >= 0)
+            {
+                if (oldPosition % 2 != 0)
+                {
+                    array[newPosition] = rowToInsert;
+                    --newPosition;
+                }
+                array[newPosition] = array[oldPosition];
+                --oldPosition; --newPosition;
+            }
+        }
+    }
+}
