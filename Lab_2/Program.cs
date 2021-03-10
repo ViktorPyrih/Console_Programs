1fea6aa73ae7e6d99600b5593a18337385b29715
Denis Kozlovskyi <kozlovskyi.d@gmail.com>
2021-03-10 04:17:10 +0200

    Improved entry point

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -1,10 +1,5 @@
-﻿using Lab_2.Classes;
+﻿using System;
 using Lab_2.Classes.Tasks;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab_2
 {
@@ -13,44 +8,106 @@ namespace Lab_2
         static void Main(string[] args)
         {
             // Entry point
-
-            Console.WriteLine("Print number of task you want run.");
-            var n = Console.ReadLine();
-
-            IRunnable _Task = null;
-
-            switch (n)
+            while (true)
             {
-                case "18":
-                    _Task = new Task__1_8();
-                    break;
+                try
+                {
+                    BlockChoice();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong!=(");
+                }
+            }
+        }
 
-                case "28":
-                    _Task = new Task__2_8();
-                    break;
+        static void BlockChoice()
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose block to run (print zero to exit):");
+                
+                if (!int.TryParse(Console.ReadLine(), out int block)) 
+                    continue;
+                
+                switch (block)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        TaskChoice(1);
+                        break;
+                    case 2:
+                        TaskChoice(2);
+                        break;
+                    case 3:
+                        TaskChoice(3);
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
 
-                case "38":
-                    _Task = new Task__3_8();
-                    break;
+        private static void TaskChoice(int block)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose person from below:");
+                Console.WriteLine("1 - Igor\n2 - Denis");
+                
+                if(!int.TryParse(Console.ReadLine(), out int task)) 
+                    continue;
+                
+                switch (task)
+                {
+                    case 1:
+                        IgorTasks(block);
+                        break;
+                    case 2:
+                        DenisTasks(block);
+                        break;
+                    default:
+                        continue;
+                } 
+            }
+        }
 
-                case "110":
-                    _Task = new Task__1_10();
+        private static void IgorTasks(int block)
+        {
+            switch (block)
+            {
+                case 1:
+                    new Task__1_8().Run();
                     break;
-
-                case "210":
-                    _Task = new Task__2_10();
+                case 2:
+                    new Task__2_8().Run();
                     break;
-
-                case "310":
-                    _Task = new Task__3_10();
-                    break;
-
-                default:
-                    Main(new string[0]);
+                case 3:
+                    new Task__3_8().Run();
                     break;
             }
 
-            _Task?.Run();
+            BlockChoice();
+        }
+        
+        private static void DenisTasks(int block)
+        {
+            switch (block)
+            {
+                case 1:
+                    new Task__1_10().Run();
+                    break;
+                case 2:
+                    new Task__2_10().Run();
+                    break;
+                case 3:
+                    new Task__3_10().Run();
+                    break;
+            }
+
+            BlockChoice();
         }
     }
 }
