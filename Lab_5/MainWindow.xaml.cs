41f49731e149f17285d8a8e5a0aa278ba24ffa95
kallDun <igorxlonline24@gmail.com>
2021-04-21 14:19:36 +0300

    Lab 5 commit Not End

---
+++ Lab_5/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OnLoad();
+            new Thread(Draw).Start();
+            new Thread(Update).Start();
+        }
+
+        private void OnLoad()
+        {
+            DrawingVisual dv = new DrawingVisual();
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                DrawPrimitives(dc);
+            }
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)Width, (int)Height, 96, 96, PixelFormats.Pbgra32);
+            rtb.Render(dv);
+
+            PictureBox.Source = rtb;
+        }
+
+        private void Draw()
+        {
+            
+        }
+
+        private void Update()
+        {
+
+        }
+
+    }
+}
---
+++ Lab_5/Tasks/Task_A.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Lab_5
+{
+    public partial class MainWindow
+    {
+
+        public void DrawPrimitives(DrawingContext dc)
+        {
+            dc.DrawRectangle(Brushes.Red, null, new Rect(20, 20, 150, 100));
+
+            GeometryDrawing arc = CreateArcDrawing(new Point(35, 35), radius: 10, 40, 100);
+            dc.DrawGeometry(Brushes.Blue, new Pen(Brushes.Gray, 10), arc.Geometry);
+        }
+
+        private GeometryDrawing CreateArcDrawing(Point point, int radius, int degree_start, int degree_sweep)
+        {
+            var startRadians = degree_start * Math.PI / 180.0;
+            var sweepRadians = degree_sweep * Math.PI / 180.0;
+
+            var xs = radius * 2
+
+
+            StreamGeometry streamGeometry = new StreamGeometry();
+            using (StreamGeometryContext ctx = streamGeometry.Open())
+            {
+                SweepDirection sweepDirection = SweepDirection.Clockwise;
+
+                ctx.BeginFigure(new Point())
+            }
+
+        }
+    }
+}
